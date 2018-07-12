df2e8ef4983358ba66c394f2e457e66ad67aee88
Gelaw <Gelaw974@gmail.com>
2018-07-12 13:39:58 +0200

    worked on recipe

--- item.lua
+++ item.lua
@@ -6,13 +6,13 @@ function Item:new(dataID, quantity)
   item.quantity = quantity
 
   function item:getName()
-    return Item.data[item.dataID]["name"]
+    return data[item.dataID]["name"]
   end
 
   return item
 end
 
-Item.data = {{id = 1, name = "default", type = 1},
+local data = {{id = 1, name = "default", type = 1},
             {id = 2, name = "wood", type = 2},
             {id = 3, name = "stone", type = 2},
             {id = 4, name = "iron ore", type = 2},
@@ -23,16 +23,16 @@ Item.data = {{id = 1, name = "default", type = 1},
             {id = 9, name = "stone furnace", type = 3, subtype = 2},
             {id = 10, name = "stone pickaxe", type = 4, subtype = 3}}
 
-Item.itemType = {{id = 1, name = "default"},
-                {id = 2, name = "ressource"},
-                {id = 3, name = "machine"},
-                {id = 4, name = "tool"}}
+local typeData = {{id = 1, name = "default"},
+                  {id = 2, name = "ressource"},
+                  {id = 3, name = "machine"},
+                  {id = 4, name = "tool"}}
 
-Item.machineSubtypes = {{id = 1, type = "default"},
-                    {id = 2, type = "furnace"},
-                    {id = 3, type = "press"}}
+local machineSubtypeData = {{id = 1, type = "default"},
+                            {id = 2, type = "furnace"},
+                            {id = 3, type = "press"}}
 
-Item.toolSubtypes = {{id = 1, type = "default"},
-                  {id = 2, type = "axe"},
-                  {id = 3, type = "pickaxe"}}
+local toolSubtypeData = {{id = 1, type = "default"},
+                        {id = 2, type = "axe"},
+                        {id = 3, type = "pickaxe"}}
 return Item
---
+++ recipe.lua
@@ -0,0 +1,8 @@
+local Item = require "item"
+local Recipe = {}
+
+  local data =
+  {{id = 1,machineSubtypeData = 2, {{itemID = 4, quantity = 2}}, {{itemID = 6, quantity = 1}}},
+  {id = 2, {{itemID = 5, quantity = 2}}, {{itemID = 7, quantity = 1}}}}
+
+return Recipe
